85024d91bfae07372690b31c8eb4a0fdfbb3a93f
govindraj111 <govindraj7695@gmail.com>
2020-10-06 23:58:08 +0530

    Fibonacci Series in Java added

---
+++ Java/FibonacciSeries.java
@@ -0,0 +1,33 @@
+/** 
+ * Recursive implementation of 
+ * Fibonacci Series
+*/
+import java.util.Scanner;
+
+class FibonacciSeries {
+  
+    // method to print the series 
+    static int fib(int n) { 
+        // Base Case 
+        if (n <= 1) 
+            return n; 
+  
+        // Recursive call 
+        return fib(n - 1) 
+            + fib(n - 2); 
+    } 
+  
+    // main method 
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+                
+        // N series 
+        int N = input.nextInt(); 
+  
+        // Print the first N numbers 
+        for (int i = 0; i < N; i++) {
+            System.out.print(fib(i) + " "); 
+        } 
+    } 
+} 
