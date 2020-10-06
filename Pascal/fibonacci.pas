ffd2d6a48553d0542ec8292a56e4c5d22234691d
Michael-Krause <michael.krause@gal-digital.de>
2020-10-07 00:27:43 +0200

    add implementation of the fibonacci sequence in Pascal

---
+++ Pascal/fibonacci.pas
@@ -0,0 +1,57 @@
+(*
+  You could run this code with an online compiler e.g. https://www.tutorialspoint.com/compile_pascal_online.php
+
+  @author: michaeldonath
+*)
+PROGRAM fibonacci;
+
+TYPE
+  IntArray = array of Integer;
+
+VAR
+  number: Integer;
+  index: Integer;
+  sequence: IntArray;
+
+PROCEDURE print(result: IntArray);
+VAR
+  size, index: Integer;
+BEGIN
+  size := Length(result);
+  FOR index := 0 TO size DO
+  BEGIN
+    Writeln (result[index]);
+  END
+END;
+
+FUNCTION fib(N: INTEGER): INTEGER;
+VAR
+  U, V, W, I: INTEGER;
+BEGIN
+  IF N <= 0 THEN
+    fib := 0
+  ELSE IF N = 1 THEN
+    fib := 1
+  ELSE BEGIN
+    U := 0;
+    V := 1;
+    FOR I:=2 TO N DO
+    BEGIN
+      W := U + V;
+      U := V;
+      V := W;
+    END;
+    fib := V;
+  END;
+END;
+
+BEGIN
+  Writeln ('CALCULATE THE NTH VALUE F(N) FOR N=:');
+  Readln (number);
+  SetLength(sequence, number);
+  FOR index := 0 TO number DO
+  BEGIN
+    sequence[index] := fib(index)
+  END;
+  print(sequence)
+END.
