d0a32a640df4cd9e05290e2bb1aec0320365fcc4
sanyud24 <57134307+sanyud24@users.noreply.github.com>
2020-10-06 23:21:47 +0530

    upto 10 terms
    upto 10 terms

--- C/fibonacci series.c
+++ C/fibonacci series.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main() {
-    int i, n, a = 0, b = 1, nextTerm;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: ");
+    int i,  a = 0, b = 1, nextTerm;
+   
+    printf("Fibonacci Series of first 10 terms is : ");
 
-    for (i = 1; i <= n; ++i) {
+    for (i = 1; i <= 10; ++i) {
         printf("%d, ", a);
         nextTerm = a + b;
         a = b;
