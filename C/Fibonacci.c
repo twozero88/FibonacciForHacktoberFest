b52883292936dc062821d6d9adcdfe2578b25f57
StephenTrainor <stephontrainor@gmail.com>
2020-10-07 10:05:32 -0700

    Changed iterations to 10

--- C/Fibonacci.c
+++ C/Fibonacci.c
@@ -5,18 +5,12 @@ int main(void) {
     int x = 0;
     int y = 1;
     int z = 0;
-    printf("Enter the number of terms: ");
-    scanf("%i", &terms);
-    if (terms < 1) {
-        printf("Enter in a valid input next time.");
-    }
-    else {
-        for (int i = 0; i < terms; i++) {
-            printf("%i ", x);
-            z = x;
-            x = y;
-            y += z;
-        }
+    int terms = 10;
+    for (int i = 0; i < terms; i++) {
+        printf("%i ", x);
+        z = x;
+        x = y;
+        y += z;
     }
     printf("\n");
 }
