361c810accec794c3a4d4907ceab5259e4f7d99d
Joonas Moilanen <joonasmmm@gmail.com>
2020-10-07 13:45:48 +0300

    Added fibonacci solution for C#

---
+++ Csharp/FibonacciSequence.cs
@@ -0,0 +1,27 @@
+using System;  
+
+  public class FibonacciExample  
+   {  
+     public static void Main(string[] args)  
+      {  
+         int n1=0,n2=1,n3,i,number;   
+
+         Console.Write("Enter the number of elements: ");    
+         number = int.Parse(Console.ReadLine());  
+
+         Console.WriteLine("We always start from: "+n1+" "+n2+" "); //printing 0 and 1  
+
+
+         for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+         {    
+          n3=n1+n2;    
+          Console.WriteLine(n3+" ");    
+          n1=n2;    
+          n2=n3;    
+         } 
+
+         Console.WriteLine("Press 'any' key to shutdown exe...");
+         // for exe to not shutdown
+         Console.ReadLine();   
+      }  
+   } 
